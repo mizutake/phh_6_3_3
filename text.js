cda2e9d5d6ab76afd3ced2fc26647cc5ab2778b8
mizutake <takeshi_17n1100069@.nnn.ed.jp>
2017-06-26 17:22:56 +0900

    First commit

---
+++ text.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('result_text').textContent = 
+    '<a href="http://www.wings.msu.to/">WINGSプロジェクト</a>';
+    document.getElementById('result_html').innerHTML =
+    '<a href="http://www.wings.msu.to/">WINGSプロジェクト</a>';
+},false);
