78e8c9e55207ba7b876872a60d5542fa67ae8628
mizutake <takeshi_17n1100069@.nnn.ed.jp>
2017-07-03 15:50:06 +0900

    リスト6-17

--- text.js
+++ text.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('result_text').textContent = 
-    '<a href="http://www.wings.msu.to/">WINGSプロジェクト</a>';
-    document.getElementById('result_html').innerHTML =
-    '<a href="http://www.wings.msu.to/">WINGSプロジェクト</a>';
+    document.getElementById('btn').addEventListener('click', function() {
+        let name = document.getElementById('name');
+        let result = document.getElementById('result');
+        //result.innerHTML = 'こんにちは、' + name.value + 'さん!';
+        result.textContent = 'こんにちは、' + name.value + 'さん!';
+    }, false);
 },false);
+
+//<div onclick="anert('首を出せ')">山の翁</div>
